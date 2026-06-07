8cefd3b5b7ef61e3d0dff7107914cf57637d3df7
Trung Nguyen <trung.nguyen@msn.com>
2026-06-07 20:43:33 +0700

    feat: Init project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <>
+      <section className='intro'>
+        <h1>Scrolling May Cause Joy</h1>
+      </section>
+
+      <section className='spot-light'>
+        <div className='svg-container'></div>
+        <div className='spotlight-header'>
+          <div>Insert Something Wildly Impressive Right Here</div>
+        </div>
+      </section>
+
+      <section className='outro'>
+        <h1>Thanks for scrolling!</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
